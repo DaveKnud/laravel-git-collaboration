ba97d77193528088aa9f3743813cd2042b2935ff
gabrielemafrica <gabriele.ga@live.it>
2023-07-21 22:54:22 +0300

    cards

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,23 +1,27 @@
 @extends('layout.main-layout')
 
 @section('content')
-    <ul>
+    <div class="container d-flex flex-wrap gap-4 mt-5">
         @foreach ($vans as $van)
-            <li>{{ $van->brand }}
-                <p>{{ $van->description }}</p>
-                <img src="{{ $van->image }}" alt="">
-            </li>
-            <li>{{ $van->brand }}</li>
-            @if ($van->color)
-                <li>
+            <div class="card text-bg-secondary mb-3" style="max-width: 18rem;">
+                <div class="card-header">{{ $van->brand }}</div>
+                <div class="card-body">
+                    <h5 class="card-title">Descrizione</h5>
+                    <p class="card-text">{{ $van->description }}</p>
+                    <h5 class="card-title">Colore</h5>
                     <span class="color-square" style="background-color: {{ $van->color }}"></span>
-                    {{ $van->color }}
-                </li>
-            @endif
-            <li>{{ $van->automatic_gear ? 'V' : 'F' }}</li>
-            <li>{{ $van->release_date }}</li>
-            <li>{{ $van->weight }}</li>
-            <li>{{ $van->engineCapacity }}</li>
+                    <span>{{ $van->color }}</span>
+                    <span class="color-square" style="background-color: {{ $van->color }}"></span>
+                    <h5 class="card-title">Cambio</h5>
+                    <p class="card-text">{{ $van->automatic_gear ? 'automatico' : 'manuale' }}</p>
+                    <h5 class="card-title">Data</h5>
+                    <p class="card-text">{{ $van->release_date }}</p>
+                    <h5 class="card-title">Peso</h5>
+                    <p class="card-text">{{ $van->weight }} kg</p>
+                    <h5 class="card-title">Serbatoio</h5>
+                    <p class="card-text">{{ $van->engineCapacity }} litri</p>
+                </div>
+            </div>
         @endforeach
-    </ul>
+    </div>
 @endsection
